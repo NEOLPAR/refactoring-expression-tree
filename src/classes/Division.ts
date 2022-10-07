e1a159da11ab5daa00057f91a01ea344937766c1
NEOLPAR <ricardo@neolpar.com>
2022-10-07 21:23:26 +0100

    division class

---
+++ src/classes/Division.ts
@@ -0,0 +1,15 @@
+import Operation from './Operation'
+
+export default class Division extends Operation {
+  constructor(left: number, right: number) {
+    super('/', left, right);
+  }
+
+  public result(): number {
+    if ( this.left === 0 || this.right === 0 ) {
+      return 0
+    }
+
+    return this.left / this.right;
+  }
+}
